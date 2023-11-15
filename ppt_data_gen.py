6256049b1cc25f60707d4aa9232ad4524b9b2d1b
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-16 05:24:45 +0530

    single prompt approach for content slide generation

--- ppt_data_gen.py
+++ ppt_data_gen.py
@@ -80,22 +80,72 @@ from langchain.prompts import PromptTemplate
 llm = Ollama(model="dolphin2.1-mistral",
              temperature="0.6")
 
-llm_low_temp = Ollama(model="dolphin2.1-mistral",
-                      temperature="0")
+# llm_low_temp = Ollama(model="dolphin2.1-mistral",
+#                       temperature="0")
 
 
-def extract_items(text):
+def extract_title_items(text):
     pattern = r'<<\s*"([^"]+)"\s*\|\s*"([^"]+)"\s*>>'
     matches = re.findall(pattern, text)
     return [item for match in matches for item in match]
 
 
-print(extract_items(llm("""
+def extract_items(input_string):
+    # Find the text inside the << >>
+    content = re.search(r'<<(.+?)>>', input_string)
+
+    if content:
+        content = content.group(1)
+    else:
+        return []
+
+    # Split the content by the | separator and remove whitespace
+    items = [item.strip() for item in content.split('|')]
+
+    # Remove the quotes from each item
+    items = [re.sub(r'^"|"$', '', item) for item in items]
+
+    return items
+
+
+topic = "Pentesting in software development"
+
+slide_data = []
+
+slide_data.append(extract_title_items(llm(f"""
 You are a text summarization and formatting specialized model that fetches relevant information
 
-For the topic "Ethics in Business" suggest a presentation title and a presentation subtitle it should be returned in the format :
+For the topic "{topic}" suggest a presentation title and a presentation subtitle it should be returned in the format :
 << "title" | "subtitle >>
 
 example :
 << "Ethics in Design" | "Integrating Ethics into Design Processes" >>
 """)))
+
+slide_data.append(extract_items(llm(f"""
+You are a text summarization and formatting specialized model that fetches relevant information
+          
+For the presentation titles "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
+Write a table of contents containing the title of each slide for a 7 slide presentation
+It should be of the format :
+<< "slide1" | "slide2" | "slide3" | ... | >>
+          
+example :
+<< "Introduction" | "User-Centered Design" | "Transparency and Honesty" | "Data Privacy and Security" | "Accessibility and Inclusion" | "Social Impact and Sustainability" | "Ethical AI and Automation" | "Collaboration and Professional Ethics" >>          
+          """)))
+
+for subtopic in slide_data[1]:
+    print((llm(f"""
+You are a text summarization and formatting specialized model that fetches relevant information
+          
+For the presentation titles "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
+Write the contents for a slide with the subtopic {subtopic}
+Make the points short , concise and to the point . Please expand the points also , each slide should contain 5 points , which are 10 words maximum
+
+It should be of the format :
+<< "point1" | "point2" | "point3" | ... | >>
+          
+example :
+<< "Foster a collaborative and inclusive work environment." | "Respect intellectual property rights and avoid plagiarism." | "Uphold professional standards and codes of ethics." | "Be open to feedback and continuous learning." >>
+          """)))
+    print()
--- todo.md
+++ todo.md
@@ -1,2 +1,3 @@
 # To Do
 - figure out a method to set font color in master slide itself
+- understand regex better and implement that for text extraction functions
