fd1039b8fc7ca7b157e7866a79208a082cdaef20
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-16 06:33:55 +0530

    modularizing ppt gen script

--- ppt_gen.py
+++ ppt_gen.py
@@ -3,9 +3,6 @@ from pptx.util import Pt
 from pptx.dml.color import RGBColor
 from pptx.enum.text import PP_ALIGN
 
-
-ppt = Presentation()
-
 # Presentation Structure
 # Opening Slide - [Title,Subtitle]
 # Overview - [1,2,3,4,5,6,7,8]
@@ -73,40 +70,30 @@ slide_data = [
      "Be open to feedback and continuous learning."],
 ]
 
-# Setting Background
-slide_master = ppt.slide_master
-slide_master.background.fill.solid()
-slide_master.background.fill.fore_color.rgb = RGBColor(0, 0, 0)
 
-# Title Screen
-curr_slide = ppt.slides.add_slide(ppt.slide_layouts[0])
-curr_slide.shapes.title.text = slide_data[0][0]
-curr_slide.shapes.title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
-    255, 255, 255)
-curr_slide.shapes.placeholders[1].text = slide_data[0][1]
-curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
-    255, 255, 255)
+def ppt_gen(slide_data):
+    ppt = Presentation()
 
-# Overview
-curr_slide = ppt.slides.add_slide(ppt.slide_layouts[1])
-curr_slide.shapes.title.text = slide_data[1][0]
-curr_slide.shapes.title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
-    255, 255, 255)
-for content in slide_data[1][1:]:
-    tframe = curr_slide.shapes.placeholders[1].text_frame
-    para = tframe.add_paragraph()
-    para.text = content
-    para.level = 1
-    para.runs[0].font.color.rgb = RGBColor(
-        255, 255, 255)
+    # Setting Background
+    slide_master = ppt.slide_master
+    slide_master.background.fill.solid()
+    slide_master.background.fill.fore_color.rgb = RGBColor(0, 0, 0)
 
-# Content Slides
-for curr_slide_data in slide_data[2:]:
-    curr_slide = ppt.slides.add_slide(ppt.slide_layouts[1])
-    curr_slide.shapes.title.text = curr_slide_data[0]
+    # Title Screen
+    curr_slide = ppt.slides.add_slide(ppt.slide_layouts[0])
+    curr_slide.shapes.title.text = slide_data[0][0]
     curr_slide.shapes.title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
         255, 255, 255)
-    for content in curr_slide_data[1:]:
+    curr_slide.shapes.placeholders[1].text = slide_data[0][1]
+    curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
+        255, 255, 255)
+
+    # Overview
+    curr_slide = ppt.slides.add_slide(ppt.slide_layouts[1])
+    curr_slide.shapes.title.text = slide_data[1][0]
+    curr_slide.shapes.title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
+        255, 255, 255)
+    for content in slide_data[1][1:]:
         tframe = curr_slide.shapes.placeholders[1].text_frame
         para = tframe.add_paragraph()
         para.text = content
@@ -114,15 +101,28 @@ for curr_slide_data in slide_data[2:]:
         para.runs[0].font.color.rgb = RGBColor(
             255, 255, 255)
 
+    # Content Slides
+    for curr_slide_data in slide_data[2:]:
+        curr_slide = ppt.slides.add_slide(ppt.slide_layouts[1])
+        curr_slide.shapes.title.text = curr_slide_data[0]
+        curr_slide.shapes.title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(
+            255, 255, 255)
+        for content in curr_slide_data[1:]:
+            tframe = curr_slide.shapes.placeholders[1].text_frame
+            para = tframe.add_paragraph()
+            para.text = content
+            para.level = 1
+            para.runs[0].font.color.rgb = RGBColor(
+                255, 255, 255)
 
-# Thank You Screen
-curr_slide = ppt.slides.add_slide(ppt.slide_layouts[2])
-curr_slide.shapes.placeholders[1].text = "Thank You"
+    # Thank You Screen
+    curr_slide = ppt.slides.add_slide(ppt.slide_layouts[2])
+    curr_slide.shapes.placeholders[1].text = "Thank You"
 
-curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].font.color.rgb = RGBColor(
-    255, 255, 255)
-curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].font.size = Pt(96)
-curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].font.color.rgb = RGBColor(
+        255, 255, 255)
+    curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].font.size = Pt(
+        96)
+    curr_slide.shapes.placeholders[1].text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
 
-ppt.save(f"{slide_data[0][0]}.pptx")
-print("done")
+    ppt.save(f"{slide_data[0][0]}.pptx")
