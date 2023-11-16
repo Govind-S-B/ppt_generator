2b4f0e9bd3ab2a2beeaa12b827b96a865e2c5726
Govind-S-B <b.s.dnivog@gmail.com>
2023-11-16 06:30:04 +0530

    cleaned up Spaghetti code

--- ppt_data_gen.py
+++ ppt_data_gen.py
@@ -1,15 +1,5 @@
 import re
 from langchain.llms import Ollama
-from langchain.prompts import PromptTemplate
-
-llm = Ollama(model="dolphin2.1-mistral",
-             temperature="0.4")
-
-
-def extract_title_items(text):
-    pattern = r'<<\s*"([^"]+)"\s*\|\s*"([^"]+)"\s*>>'
-    matches = re.findall(pattern, text)
-    return [item for match in matches for item in match]
 
 
 def extract_items(input_string):
@@ -30,57 +20,62 @@ def extract_items(input_string):
     return items
 
 
-topic = "Pentesting in software development"
+def slide_data_gen(topic):
+    llm = Ollama(model="dolphin2.1-mistral",
+                 temperature="0.4")
 
-slide_data = []
+    slide_data = []
 
-slide_data.append(extract_title_items(llm(f"""
-You are a text summarization and formatting specialized model that fetches relevant information
+    slide_data.append(extract_items(llm(f"""
+    You are a text summarization and formatting specialized model that fetches relevant information
 
-For the topic "{topic}" suggest a presentation title and a presentation subtitle it should be returned in the format :
-<< "title" | "subtitle >>
+    For the topic "{topic}" suggest a presentation title and a presentation subtitle it should be returned in the format :
+    << "title" | "subtitle >>
 
-example :
-<< "Ethics in Design" | "Integrating Ethics into Design Processes" >>
-""")))
+    example :
+    << "Ethics in Design" | "Integrating Ethics into Design Processes" >>
+    """)))
 
-slide_data.append(extract_items(llm(f"""
-You are a text summarization and formatting specialized model that fetches relevant information
-          
-For the presentation titled "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
-Write a table of contents containing the title of each slide for a 7 slide presentation
-It should be of the format :
-<< "slide1" | "slide2" | "slide3" | ... | >>
-          
-example :
-<< "Introduction" | "User-Centered Design" | "Transparency and Honesty" | "Data Privacy and Security" | "Accessibility and Inclusion" | "Social Impact and Sustainability" | "Ethical AI and Automation" | "Collaboration and Professional Ethics" >>          
-          """)))
+    slide_data.append(extract_items(llm(f"""
+    You are a text summarization and formatting specialized model that fetches relevant information
+            
+    For the presentation titled "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
+    Write a table of contents containing the title of each slide for a 7 slide presentation
+    It should be of the format :
+    << "slide1" | "slide2" | "slide3" | ... | >>
+            
+    example :
+    << "Introduction" | "User-Centered Design" | "Transparency and Honesty" | "Data Privacy and Security" | "Accessibility and Inclusion" | "Social Impact and Sustainability" | "Ethical AI and Automation" | "Collaboration and Professional Ethics" >>          
+            """)))
 
-for subtopic in slide_data[1]:
+    for subtopic in slide_data[1]:
 
-    data_to_clean = llm(f"""
-You are a content generation specialized model that fetches relevant information and presents it in clear concise manner
-          
-For the presentation titled "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
-Write the contents for a slide with the subtopic {subtopic}
-Write 5 points. Each point 10 words maximum.
-Make the points short, concise and to the point.
-""")
+        data_to_clean = llm(f"""
+        You are a content generation specialized model that fetches relevant information and presents it in clear concise manner
+                
+        For the presentation titled "{slide_data[0][0]}" and with subtitle "{slide_data[0][1]}" for the topic "{topic}"
+        Write the contents for a slide with the subtopic {subtopic}
+        Write 5 points. Each point 10 words maximum.
+        Make the points short, concise and to the point.
+            """)
 
-    cleaned_data = llm(f"""
-You are a text summarization and formatting specialized model that fetches relevant information and formats it into user specified formats
-Given below is a text draft for a presentation slide containing 5 points , extract the 5 sentences and format it as :
-              
-<< "point1" | "point2" | "point3" | ... | >>
-              
-example :
-<< "Foster a collaborative and inclusive work environment." | "Respect intellectual property rights and avoid plagiarism." | "Uphold professional standards and codes of ethics." | "Be open to feedback and continuous learning." >>
+        cleaned_data = llm(f"""
+        You are a text summarization and formatting specialized model that fetches relevant information and formats it into user specified formats
+        Given below is a text draft for a presentation slide containing 5 points , extract the 5 sentences and format it as :
+                    
+        << "point1" | "point2" | "point3" | ... | >>
+                    
+        example :
+        << "Foster a collaborative and inclusive work environment." | "Respect intellectual property rights and avoid plagiarism." | "Uphold professional standards and codes of ethics." | "Be open to feedback and continuous learning." >>
 
--- Beginning of the text --
-{data_to_clean}
--- End of the text --         
-    """)
+        -- Beginning of the text --
+        {data_to_clean}
+        -- End of the text --         
+                """)
 
-    slide_data.append([subtopic] + extract_items(cleaned_data))
+        slide_data.append([subtopic] + extract_items(cleaned_data))
 
-print(slide_data)
+    return slide_data
+
+
+print(slide_data_gen("Creativity in Engineering"))
